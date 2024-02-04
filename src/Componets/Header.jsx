fb2dbf618392f227af30df1b6152e3ee7fdc2c7c
9AndrewsEsteban8 <andrewsesteban7@gmail.com>
2024-02-04 11:51:29 -0500

    header

---
+++ src/Componets/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <nav>
+            <a href="">Sub1 </a>
+            <a href="">Sub2 </a>
+            <a href="">Sub3 </a>
+            <a href="">Sub4 </a>
+            <a href="">Sub5 </a>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
